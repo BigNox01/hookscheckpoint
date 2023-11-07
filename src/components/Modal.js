0afde0763375991cb1827fba9923d0b315aafd05
BigNox <faresmaazoul4@gmail.com>
2023-11-07 16:40:24 +0100

    third version

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,28 +3,26 @@ import Modal from 'react-bootstrap/Modal';
 
 function MoviePopUp({show,onHide,movie}) {
   return (
-    <div className='movieModal'>
+
     <div
       className="modal show"
-      style={{ display: 'block', position: 'initial' }}
+      style={{ display: 'flex', alignItems:'center' }}
     >
-      <Modal show={show} onHide={onHide} ></Modal>
+      <Modal show={show} onHide={onHide} centered ></Modal>
       <Modal.Dialog>
         <Modal.Header closeButton>
           <Modal.Title>{movie.title}</Modal.Title>
         </Modal.Header>
-
         <Modal.Body>
           <p>{movie.description}</p>
         </Modal.Body>
-
         <Modal.Footer>
           <Button variant="secondary" onClick={onHide} >Close</Button>
           <Button variant="primary" href={movie.link} target='_blank' rel='noopener noreferrer' >See Trailer</Button>
         </Modal.Footer>
       </Modal.Dialog>
     </div>
-    </div>
+
 
   );
 }
